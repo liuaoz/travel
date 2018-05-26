b16c465bda5e0bd306593f6a752b903b0c249468
liuaoz <576729125@qq.com>
2018-05-26 23:04:08 +0800

    x

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,17 +2,13 @@
   <div class="wrapper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img" src="http://img.taopic.com/uploads/allimg/140321/240418-14032109294180.jpg">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url">
       </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="mother-daughter.jpg">
-      </swiper-slide>
-      <swiper-slide>I'm Slide 3</swiper-slide>
-      <swiper-slide>I'm Slide 4</swiper-slide>
-      <swiper-slide>I'm Slide 5</swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
+      <!--<div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>-->
     </swiper>
   </div>
 </template>
@@ -22,19 +18,47 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 1000
+        }
+      },
+      swiperList: [
+        {
+          id: '001',
+          url: '/static/images/mother-daughter.JPG'
+        },
+        {
+          id: '002',
+          url: '/static/images/my-parents.JPG'
+        },
+        {
+          id: '003',
+          url: '/static/images/mother-daughter.JPG'
+        },
+        {
+          id: '004',
+          url: '/static/images/with-computer.JPG'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
   .wrapper
-    overflow: hidden
-    width: 100%
-    height: 0
-    padding-bottom: 31.25%
-    background: #eee
+    overflow : hidden
+    height : 0
+    width :100%
+    padding-bottom : 75%
+    background : #eee
     .swiper-img
-    width: 100%
+      width : 100%
 </style>
