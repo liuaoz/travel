4a2f2e56cb61378de5da5c323d38b61455480d5a
liuaoz <576729125@qq.com>
2018-05-27 16:22:41 +0800

    recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
   <home-header></home-header>
   <home-swiper></home-swiper>
   <home-icons></home-icons>
+  <home-recommend></home-recommend>
   <div>Welcome to my home.</div>
   <home-footer></home-footer>
 </div>
@@ -13,13 +14,16 @@
 import HomeHeader from './components/Header'
 import HomeFooter from './components/Footer'
 import HomeSwiper from './components/Swiper'
+import HomeRecommend from './components/Recommend'
 import HomeIcons from './components/Icons'
+
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
+    HomeRecommend,
     HomeFooter
   }
 }
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="title">热门推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of itemList" :key="item.id">
+        <img class="item-img" :src="item.url"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      itemList: [
+        {
+          id: '0001',
+          url: '/static/images/mother-daughter.JPG',
+          title: '大澳湾海景',
+          desc: '这个地方可以海上游玩'
+        },
+        {
+          id: '0002',
+          url: '/static/images/mother-daughter.JPG',
+          title: '大澳湾海景',
+          desc: '这个地方可以海上游玩'
+        },
+        {
+          id: '0003',
+          url: '/static/images/mother-daughter.JPG',
+          title: '大澳湾海景',
+          desc: '这个地方可以海上游玩'
+        },
+        {
+          id: '0004',
+          url: '/static/images/mother-daughter.JPG',
+          title: '大澳湾海景',
+          desc: '这个地方可以海上游玩'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top : .2rem
+    line-height : .8rem
+    background : #eee
+    text-indent : .2rem
+  .item
+    display : flex
+    height : 1.9rem
+    .item-img
+      width : 1.7rem
+      height : 1.7rem
+      padding : .1rem
+    .item-info
+      flex: 1
+      padding : .1rem
+      min-width : 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height : .44rem
+        margin-top : .16rem
+        background : #ff9300
+        padding : 0 .2rem
+        border-radius : .06rem
+        color : #fff
+</style>
