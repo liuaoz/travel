d81c6ea941fa52ca1e1fca2bd0c646f392405674
chenxianxi <576729125@qq.com>
2018-05-29 09:38:24 +0800

    ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 
 export default {
   name: 'City',
@@ -20,6 +21,29 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      hotCities: [],
+      cities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        this.hotCities = res.data.hotCities
+        this.cities = res.data.cities
+      }
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
